ea123e638e17b0fee5f9507c55239d02e1e1e503
wwan13 <wwan13@naver.com>
2023-10-05 01:42:54 +0900

    feat : 기본 예외 처리 방법 정의

---
+++ common/src/main/java/io/wwan13/common/exeption/BusinessException.java
@@ -0,0 +1,10 @@
+package io.wwan13.common.exeption;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
