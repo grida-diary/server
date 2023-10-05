80388a1a346d99fc54102e5fce6bbbb694fff185
wwan13 <wwan13@naver.com>
2023-10-05 14:09:55 +0900

    refactor : 모든 예외가 BaseException 을 상속 받도록 변경

--- common/src/main/java/io/wwan13/common/exeption/base/BaseException.java
+++ common/src/main/java/io/wwan13/common/exeption/base/BaseException.java
@@ -1,10 +1,10 @@
-package io.wwan13.common.exeption;
+package io.wwan13.common.exeption.base;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class BusinessException extends RuntimeException {
+public abstract class BaseException extends RuntimeException {
     private final ErrorCode errorCode;
 }
