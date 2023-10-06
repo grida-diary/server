9f69ddfade2ce08160ed326f06a82aa2d305dc6e
wwan13 <wwan13@naver.com>
2023-10-07 00:19:24 +0900

    feat : 모듈 별 루트 클래스 생성

---
+++ security/src/main/java/io/wwan13/SecurityApplication.java
@@ -0,0 +1,9 @@
+package io.wwan13;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "io.wwan13")
+public class SecurityApplication {
+}
