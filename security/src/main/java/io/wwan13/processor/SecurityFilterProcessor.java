f9caae3663accfdeb7a8844235c58b6cb1543349
wwan13 <wwan13@naver.com>
2023-10-07 15:08:44 +0900

    feat : jwtFilter 401, 403 에러 대응

--- security/src/main/java/io/wwan13/processor/SecurityFilterProcessor.java
+++ security/src/main/java/io/wwan13/processor/SecurityFilterProcessor.java
@@ -1,10 +1,14 @@
 package io.wwan13.processor;
 
+import io.wwan13.jwt.JwtAccessDeniedHandler;
+import io.wwan13.jwt.JwtAuthenticationEntryPointHandler;
 import io.wwan13.jwt.JwtFilter;
+import io.wwan13.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +17,15 @@ import org.springframework.stereotype.Component;
 public class SecurityFilterProcessor extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private final JwtFilter jwtFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPointHandler jwtAuthenticationEntryPointHandler;
 
     public HttpSecurity common(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.addFilterBefore(jwtFilter, BasicAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.exceptionHandling()
+                .accessDeniedHandler(jwtAccessDeniedHandler);
+        httpSecurity.exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPointHandler);
         return httpSecurity;
     }
 }
