e8289607327a26198ed024d8f0c60681e6034bea
wwan13 <wwan13@naver.com>
2023-10-12 01:57:27 +0900

    feat : 다이어리가 하루에 하나만 작성되었는지 검증하는 기능, 예외처리

--- core/src/main/java/io/wwan13/domain/diary/exception/DiaryErrorCode.java
+++ core/src/main/java/io/wwan13/domain/diary/exception/DiaryErrorCode.java
@@ -11,7 +11,9 @@ public enum DiaryErrorCode implements ErrorCode {
 
     CONTENT_TOO_SHORT(HttpStatusCode.BAD_REQUEST, "일기의 내용이 너무 짧습니다."),
     CONTENT_TOO_LONG(HttpStatusCode.BAD_REQUEST, "일기의 내용이 너무 깁니다."),
-    DIARY_NOT_FOUND(HttpStatusCode.BAD_REQUEST, "일기를 찾을 수 없습니다.")
+    DIARY_NOT_FOUND(HttpStatusCode.BAD_REQUEST, "일기를 찾을 수 없습니다."),
+    FUTURE_DIARY(HttpStatusCode.BAD_REQUEST, "일기는 오늘 혹은 과거의 일기만 작성할 수 있습니다."),
+    DIARY_ALREADY_EXIST_IN_DATE(HttpStatusCode.BAD_REQUEST, "일기는 하루에 하나씩 작성할 수 있습니다.")
     ;
 
     private final int httpStatus;
