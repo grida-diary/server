fd4cb3ad09613904628497a479af8b1f6666e570
wwan13 <wwan13@naver.com>
2023-10-12 01:57:23 +0900

    feat : 다이어리가 하루에 하나만 작성되었는지 검증하는 기능, 예외처리

--- core/src/main/java/io/wwan13/domain/diary/service/DiaryQueryService.java
+++ core/src/main/java/io/wwan13/domain/diary/service/DiaryQueryService.java
@@ -1,11 +1,21 @@
 package io.wwan13.domain.diary.service;
 
+import io.wwan13.domain.diary.repository.DiaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-//@Service
+import java.time.LocalDate;
+
+@Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class DiaryQueryService {
+
+    private final DiaryRepository diaryRepository;
+
+    public Boolean existByMemberEmailAndDate(String memberEmail, LocalDate date) {
+        return diaryRepository.existsByOwnerEmailAndDateDate(memberEmail, date);
+    }
+
 }
