59541f7164a4e6be65b2c6fb5bc9abc9bdd5ef99
wwan13 <wwan13@naver.com>
2023-10-12 13:35:56 +0900

    refactor : 이미지 생성하는 기능을 새 API 로 분리

--- application-client/src/main/java/io/wwan13/diary/usecase/CreateDiaryUseCase.java
+++ application-client/src/main/java/io/wwan13/diary/usecase/CreateDiaryUseCase.java
@@ -2,21 +2,15 @@ package io.wwan13.diary.usecase;
 
 import io.wwan13.annotation.UseCase;
 import io.wwan13.diary.dto.DiaryCreateRequestDto;
-import io.wwan13.diary.dto.DiaryCreateResponseDto;
+import io.wwan13.diary.dto.DiaryIdResponseDto;
 import io.wwan13.domain.diary.exception.DiaryAlreadyExistInDateException;
 import io.wwan13.domain.diary.service.DiaryQueryService;
 import io.wwan13.domain.diary.service.DiaryService;
-import io.wwan13.domain.diaryimage.service.DiaryImageService;
 import io.wwan13.domain.member.entity.Member;
 import io.wwan13.domain.member.service.MemberQueryService;
-import io.wwan13.openai.model.MemberProfile;
-import io.wwan13.openai.model.ProcessResult;
-import io.wwan13.openai.processor.ChatCompletionProcessor;
-import io.wwan13.openai.processor.ImageGenerationProcessor;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @UseCase
 @RequiredArgsConstructor
@@ -24,30 +18,15 @@ public class CreateDiaryUseCase {
 
     private final DiaryService diaryService;
     private final DiaryQueryService diaryQueryService;
-    private final DiaryImageService diaryImageService;
     private final MemberQueryService memberQueryService;
-    private final ChatCompletionProcessor chatCompletionProcessor;
-    private final ImageGenerationProcessor imageGenerationProcessor;
 
-    public DiaryCreateResponseDto execute(DiaryCreateRequestDto diaryCreateRequestDto, String memberEmail) {
+    public DiaryIdResponseDto execute(DiaryCreateRequestDto diaryCreateRequestDto, String memberEmail) {
         validate(diaryCreateRequestDto, memberEmail);
+
         Member member = memberQueryService.findByEmail(memberEmail);
         Long createdDiaryId = diaryService.save(diaryCreateRequestDto.map(member));
 
-        String diaryImageUrl = generateDiaryImageUrl(diaryCreateRequestDto.getContent(), member);
-        diaryImageService.save(createdDiaryId, diaryImageUrl);
-
-        return new DiaryCreateResponseDto(createdDiaryId);
-    }
-
-    private String generateDiaryImageUrl(String content, Member member) {
-        ProcessResult processResult = chatCompletionProcessor.proceed(content);
-
-        MemberProfile memberProfile = MemberProfile.builder()
-                .gender(member.getGenderName())
-                .age(member.getAgeValue())
-                .build();
-        return imageGenerationProcessor.proceed(memberProfile, processResult);
+        return new DiaryIdResponseDto(createdDiaryId);
     }
 
     private void validate(DiaryCreateRequestDto diaryCreateRequestDto, String memberId) {
