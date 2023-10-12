3c8895bee036cfd69fd8a8986eac17b8a8bd8ef9
wwan13 <wwan13@naver.com>
2023-10-12 13:38:42 +0900

    feat : 그림의 예시 중 하나를 골라 일기에 저장하는 API

--- application-client/src/main/java/io/wwan13/diary/controller/DiaryController.java
+++ application-client/src/main/java/io/wwan13/diary/controller/DiaryController.java
@@ -1,15 +1,15 @@
 package io.wwan13.diary.controller;
 
 import io.wwan13.diary.dto.DiaryCreateRequestDto;
-import io.wwan13.diary.dto.DiaryCreateResponseDto;
+import io.wwan13.diary.dto.DiaryIdResponseDto;
+import io.wwan13.diary.dto.DiaryImageExampleResponseDto;
 import io.wwan13.diary.usecase.CreateDiaryUseCase;
+import io.wwan13.diary.usecase.GetDiaryImageExampleUseCase;
+import io.wwan13.diary.usecase.SaveDiaryImageUseCase;
 import io.wwan13.response.SuccessResponse;
 import io.wwan13.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/diary")
@@ -17,12 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class DiaryController {
 
     private final CreateDiaryUseCase createDiaryUseCase;
+    private final GetDiaryImageExampleUseCase getDiaryImageExampleUseCase;
+    private final SaveDiaryImageUseCase saveDiaryImageUseCase;
 
     @PostMapping
-    public SuccessResponse<DiaryCreateResponseDto> create(
+    public SuccessResponse<DiaryIdResponseDto> create(
             @RequestBody DiaryCreateRequestDto diaryCreateRequestDto ) {
         String memberEmail = SecurityUtil.getCurrentUserEmail();
-        DiaryCreateResponseDto response = createDiaryUseCase.execute(diaryCreateRequestDto, memberEmail);
+        DiaryIdResponseDto response = createDiaryUseCase.execute(diaryCreateRequestDto, memberEmail);
+        return SuccessResponse.created(response);
+    }
+
+    @GetMapping("/{diaryId}/example")
+    public SuccessResponse<DiaryImageExampleResponseDto> getExampleImage(
+            @PathVariable Long diaryId) {
+        String memberEmail = SecurityUtil.getCurrentUserEmail();
+        DiaryImageExampleResponseDto response = getDiaryImageExampleUseCase.execute(diaryId, memberEmail);
+        return SuccessResponse.ok(response);
+    }
+
+    @PostMapping("/{diaryId}")
+    public SuccessResponse<DiaryIdResponseDto> saveDiaryImage(
+            @PathVariable Long diaryId,
+            @RequestBody String diaryImageUrl) {
+        DiaryIdResponseDto response = saveDiaryImageUseCase.execute(diaryId, diaryImageUrl);
         return SuccessResponse.created(response);
     }
 }
