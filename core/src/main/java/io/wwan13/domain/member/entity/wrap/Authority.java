741e8ed56b67e94c5a35feb0e1e00e19ea0a4e19
wwan13 <wwan13@naver.com>
2023-10-12 13:41:23 +0900

    feat : 일반 유저, 프로 유저 구분하여 새로고침 회수 차등 분배

--- core/src/main/java/io/wwan13/domain/member/entity/wrap/Authority.java
+++ core/src/main/java/io/wwan13/domain/member/entity/wrap/Authority.java
@@ -1,5 +1,14 @@
 package io.wwan13.domain.member.entity.wrap;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Authority {
-    ROLE_USER, ROLE_ADMIN;
+    ROLE_USER(1),
+    ROLE_PRO_USER(5),
+    ROLE_ADMIN(99);
+
+    private final Integer refreshChance;
 }
