082f726b5bd774cfe43e434cd92f84de6ace0cb7
wwan13 <wwan13@naver.com>
2023-12-31 15:36:32 +0900

    feat : infrastructure 모듈 테스트 환경 설정

---
+++ infrastructure/src/test/java/io/wwan13/TestConfiguration.java
@@ -0,0 +1,13 @@
+package io.wwan13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class TestConfiguration {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
