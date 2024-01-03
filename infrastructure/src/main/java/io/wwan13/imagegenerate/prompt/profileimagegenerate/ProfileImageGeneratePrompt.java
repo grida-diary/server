7eaa89270501de7b6695df3558f2de4542f9f34b
wwan13 <wwan13@naver.com>
2024-01-03 15:49:06 +0900

    fix : @Component 가 없언 오류 수정

--- infrastructure/src/main/java/io/wwan13/imagegenerate/prompt/profileimagegenerate/ProfileImageGeneratePrompt.java
+++ infrastructure/src/main/java/io/wwan13/imagegenerate/prompt/profileimagegenerate/ProfileImageGeneratePrompt.java
@@ -2,7 +2,9 @@ package io.wwan13.imagegenerate.prompt.profileimagegenerate;
 
 import io.wwan13.imagegenerate.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProfileImageGeneratePrompt extends Prompt {
 
     public ProfileImageGeneratePrompt(
