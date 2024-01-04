56e67fb7b799fcd326128e5868833a146a59db99
wwan13 <wwan13@naver.com>
2024-01-04 19:55:55 +0900

    test : `DiaryImageGenerator` 테스트 코드 작성 - 일기 이미지를 생성할 수 있다.

---
+++ infrastructure/src/test/java/io/wwan13/imagegenerate/generator/DiaryImageGeneratorTest.java
@@ -0,0 +1,62 @@
+package io.wwan13.imagegenerate.generator;
+
+import io.wwan13.imagegenerate.processor.image.ImageProcessResult;
+import io.wwan13.imagegenerate.processor.image.ImageProcessor;
+import io.wwan13.imagegenerate.processor.naturallanguage.NaturalLanguageProcessResult;
+import io.wwan13.imagegenerate.processor.naturallanguage.NaturalLanguageProcessor;
+import io.wwan13.imagegenerate.prompt.diaryimagegenerate.DiaryImageGeneratePrompt;
+import io.wwan13.imagegenerate.prompt.naturallanguageprocess.NaturalLanguageProcessPrompt;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayName("DiaryImageGenerator 는")
+class DiaryImageGeneratorTest {
+
+    static class MockNaturalLanguageProcessor implements NaturalLanguageProcessor {
+
+        @Override
+        public NaturalLanguageProcessResult proceed(String prompt) {
+            return new NaturalLanguageProcessResult("emotion", "behavior");
+        }
+    }
+
+    static class MockImageProcessor implements ImageProcessor {
+
+        @Override
+        public ImageProcessResult proceed(String prompt, int n) {
+            return new ImageProcessResult(List.of("imageUrl.com"));
+        }
+    }
+
+    @Test
+    void 일기_이미지를_생성할_수_있다() {
+        // given
+        NaturalLanguageProcessPrompt naturalLanguageProcessPrompt = new NaturalLanguageProcessPrompt("#DIARY");
+        DiaryImageGeneratePrompt diaryImageGeneratePrompt
+                = new DiaryImageGeneratePrompt("#EMOTION #BEHAVIOR #GENDER #AGE");
+
+        NaturalLanguageProcessor naturalLanguageProcessor = new MockNaturalLanguageProcessor();
+        ImageProcessor imageProcessor = new MockImageProcessor();
+
+        DiaryImageGenerator generator = new DiaryImageGenerator(
+                naturalLanguageProcessPrompt,
+                diaryImageGeneratePrompt,
+                naturalLanguageProcessor,
+                imageProcessor
+        );
+
+        // when
+        String imageUrl = generator.generate("diary", "man", 21);
+
+        // then
+        assertThat(imageUrl).isEqualTo("imageUrl.com");
+    }
+}
