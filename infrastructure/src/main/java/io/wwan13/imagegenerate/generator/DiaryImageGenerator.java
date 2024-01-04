aac524af5adcacae4febfe30816215fdf2f4a064
wwan13 <wwan13@naver.com>
2024-01-04 19:56:22 +0900

    feat : `DiaryImageGenerator` 기능 구현 - 일기 이미지를 생성할 수 있다.

---
+++ infrastructure/src/main/java/io/wwan13/imagegenerate/generator/DiaryImageGenerator.java
@@ -0,0 +1,54 @@
+package io.wwan13.imagegenerate.generator;
+
+import io.wwan13.imagegenerate.processor.image.ImageProcessResult;
+import io.wwan13.imagegenerate.processor.naturallanguage.NaturalLanguageProcessResult;
+import io.wwan13.imagegenerate.processor.naturallanguage.NaturalLanguageProcessor;
+import io.wwan13.imagegenerate.processor.image.ImageProcessor;
+import io.wwan13.imagegenerate.prompt.diaryimagegenerate.DiaryImageGenerateKeywords;
+import io.wwan13.imagegenerate.prompt.diaryimagegenerate.DiaryImageGeneratePrompt;
+import io.wwan13.imagegenerate.prompt.naturallanguageprocess.NaturalLanguageProcessKeywords;
+import io.wwan13.imagegenerate.prompt.naturallanguageprocess.NaturalLanguageProcessPrompt;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DiaryImageGenerator {
+
+    private static final int IMAGE_AMOUNT = 1;
+
+    private final NaturalLanguageProcessPrompt naturalLanguageProcessPrompt;
+    private final DiaryImageGeneratePrompt diaryImageGeneratePrompt;
+    private final NaturalLanguageProcessor naturalLanguageProcessor;
+    private final ImageProcessor imageProcessor;
+
+    public String generate(String diary, String gender, int age) {
+        NaturalLanguageProcessResult naturalLanguageProcessResult = analyzeEmotionAndBehavior(diary);
+        ImageProcessResult imageProcessResult = createDiaryImage(naturalLanguageProcessResult, gender, age);
+
+        return imageProcessResult.getUrl();
+    }
+
+    private NaturalLanguageProcessResult analyzeEmotionAndBehavior(String diary) {
+        NaturalLanguageProcessKeywords keywords = NaturalLanguageProcessKeywords.builder()
+                .diary(diary)
+                .build();
+
+        return naturalLanguageProcessor.proceed(naturalLanguageProcessPrompt.create(keywords));
+    }
+
+    private ImageProcessResult createDiaryImage(NaturalLanguageProcessResult naturalLanguageProcessResult,
+                                                String gender,
+                                                int age) {
+        DiaryImageGenerateKeywords keywords = DiaryImageGenerateKeywords.builder()
+                .emotion(naturalLanguageProcessResult.getEmotion())
+                .behavior(naturalLanguageProcessResult.getBehavior())
+                .gender(gender)
+                .age(age)
+                .build();
+
+        return imageProcessor.proceed(diaryImageGeneratePrompt.create(keywords), IMAGE_AMOUNT);
+    }
+}
