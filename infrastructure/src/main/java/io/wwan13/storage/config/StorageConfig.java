c3cb3b18b608a41381877a2d47241b9d8b9ffdb2
wwan13 <wwan13@naver.com>
2024-01-08 23:24:30 +0900

    refactor : `Storage` 빈 등록 방법 변경 - @Component -> @Configuration

---
+++ infrastructure/src/main/java/io/wwan13/storage/config/StorageConfig.java
@@ -0,0 +1,28 @@
+package io.wwan13.storage.config;
+
+import com.amazonaws.services.s3.AmazonS3;
+import io.wwan13.storage.cdn.CdnProcessor;
+import io.wwan13.storage.filenamegenerator.FileNameGenerator;
+import io.wwan13.storage.filenamegenerator.uuid.UuidFileNameGenerator;
+import io.wwan13.storage.uploader.ImageUploader;
+import io.wwan13.storage.uploader.aws.S3ImageUploader;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StorageConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ImageUploader imageUploader(S3Properties properties,
+                                       AmazonS3 amazonS3,
+                                       FileNameGenerator fileNameGenerator) {
+        return new S3ImageUploader(properties, amazonS3, fileNameGenerator);
+    }
+
+    @Bean
+    public FileNameGenerator fileNameGenerator() {
+        return new UuidFileNameGenerator();
+    }
+}
