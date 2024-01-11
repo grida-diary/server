374038860c3dfaf04cfc4a72042e2c1dce7dd750
wwan13 <wwan13@naver.com>
2024-01-11 17:30:49 +0900

    fix : @Component 어노테이션 제거

--- infrastructure/src/main/java/io/wwan13/storage/filenamegenerator/uuid/UuidFileNameGenerator.java
+++ infrastructure/src/main/java/io/wwan13/storage/filenamegenerator/uuid/UuidFileNameGenerator.java
@@ -1,11 +1,9 @@
 package io.wwan13.storage.filenamegenerator.uuid;
 
 import io.wwan13.storage.filenamegenerator.FileNameGenerator;
-import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Component
 public class UuidFileNameGenerator implements FileNameGenerator {
 
     private static final String FILE_NAME_FORMAT = "%s.%s";
