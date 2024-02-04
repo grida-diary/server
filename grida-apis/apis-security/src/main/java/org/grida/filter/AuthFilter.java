b240dfe55d5f49bd5e5e954249e3df70c1d7f402
wwan13 <wwan13@naver.com>
2024-02-05 01:45:32 +0900

    feat : `AuthFilter` 기능 구현 - 인증/인가 로직을 수행하는 인터셉터 - header의 Bearer 토큰 해석 후 요청 URI와 사용자의 role을 이용해 인가 검증

---
+++ grida-apis/apis-security/src/main/java/org/grida/filter/AuthFilter.java
@@ -0,0 +1,70 @@
+package org.grida.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.grida.exception.ApisSecurityException;
+import org.grida.jwt.TokenClaims;
+import org.grida.jwt.TokenDecoder;
+import org.grida.jwt.TokenValidator;
+import org.grida.model.RequestMatcher;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.grida.exception.ApisSecurityErrorCode.HTTP_FORBIDDEN;
+import static org.grida.exception.ApisSecurityErrorCode.HTTP_UNAUTHORIZED;
+
+@RequiredArgsConstructor
+public class AuthFilter extends OncePerRequestFilter {
+
+    private static final String USER_ID_ATTRIBUTE_KEY = "userId";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String AUTHENTICATION_TOKEN_PREFIX = "Bearer ";
+    private static final int AUTHENTICATION_TOKEN_START_INDEX = 7;
+    private static final long ANONYMOUS_USER_ID = -1L;
+    private static final String ANONYMOUS_ROLE = "ROLE_ANONYMOUS";
+
+
+    private final TokenDecoder tokenDecoder;
+    private final TokenValidator tokenValidator;
+    private final RequestMatcher requestMatcher;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        TokenClaims tokenClaims = decodeToken(request);
+        validateRequestMatches(request.getRequestURI(), tokenClaims.role());
+        if (tokenClaims.userId() != ANONYMOUS_USER_ID) {
+            request.setAttribute(USER_ID_ATTRIBUTE_KEY, tokenClaims.userId());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private TokenClaims decodeToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        if (StringUtils.hasText(bearerToken) || !bearerToken.startsWith(AUTHENTICATION_TOKEN_PREFIX)) {
+            return new TokenClaims(ANONYMOUS_USER_ID, ANONYMOUS_ROLE);
+        }
+
+        String token = bearerToken.substring(AUTHENTICATION_TOKEN_START_INDEX);
+        tokenValidator.validateToken(token);
+        return tokenDecoder.decode(token);
+    }
+
+    private void validateRequestMatches(String requestUri, String role) {
+        if (role.equals(ANONYMOUS_ROLE) && !requestMatcher.matches(requestUri, role)) {
+            throw new ApisSecurityException(HTTP_UNAUTHORIZED);
+        }
+        if (!role.equals(ANONYMOUS_ROLE) && !requestMatcher.matches(requestUri, role)) {
+            throw new ApisSecurityException(HTTP_FORBIDDEN);
+        }
+    }
+}
