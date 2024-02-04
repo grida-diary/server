929b6b2448f21fc3309acb0ee1eda9db2063c55b
wwan13 <wwan13@naver.com>
2024-02-04 23:45:18 +0900

    test : `TokenValidator` 테스트 코드 작성 - 토큰이 잘못 되었다면 예외가 발생한다. - 토큰이 만료 되었다면 예외가 발생한다.

---
+++ grida-apis/apis-security/src/test/java/org/grida/jwt/TokenValidatorTest.java
@@ -0,0 +1,56 @@
+package org.grida.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import org.grida.config.JwtProperties;
+import org.grida.exception.ApisSecurityErrorCode;
+import org.grida.exception.ApisSecurityException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.security.Key;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayName("TokenValidator 는")
+class TokenValidatorTest {
+
+    private Key key() {
+        return Keys.hmacShaKeyFor("secretkeysecretkeysecretkeysecretkey".getBytes());
+    }
+
+    private JwtProperties jwtProperties() {
+        return new JwtProperties("secretkey", -1, -1);
+    }
+
+    private TokenProvider tokenProvider() {
+        return new TokenProvider(jwtProperties(), key());
+    }
+
+    @Test
+    void 토큰이_잘못_되었다면_예외가_발생한다() {
+        // given
+        TokenValidator tokenValidator = new TokenValidator(key());
+        String token = "wrongToken";
+
+        // when, then
+        assertThatThrownBy(() -> tokenValidator.validateToken(token))
+                .isInstanceOf(ApisSecurityException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ApisSecurityErrorCode.INVALID_TOKEN);
+    }
+
+    @Test
+    void 토큰이_만료_되었다면_예외가_발생한다() {
+        // given
+        TokenValidator tokenValidator = new TokenValidator(key());
+        String token = tokenProvider().createToken(TokenType.ACCESS_TOKEN, new TokenClaims(1l, "ROLE_USER"));
+
+        // when, then
+        assertThatThrownBy(() -> tokenValidator.validateToken(token))
+                .isInstanceOf(ApisSecurityException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ApisSecurityErrorCode.EXPIRED_TOKEN);
+    }
+}
