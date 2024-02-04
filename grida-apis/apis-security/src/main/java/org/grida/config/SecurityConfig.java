c5c26449f75002e1c6a97f9c1b3776f54a668af9
wwan13 <wwan13@naver.com>
2024-02-05 01:51:19 +0900

    feat : `ApisSecurity` 모듈 설정 및 빈 등록

---
+++ grida-apis/apis-security/src/main/java/org/grida/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package org.grida.config;
+
+import io.jsonwebtoken.security.Keys;
+import org.grida.util.UserIdResolver;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.security.Key;
+import java.util.List;
+
+@Configuration
+@ConfigurationPropertiesScan(basePackages = "org.grida")
+public class SecurityConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new UserIdResolver());
+    }
+
+    @Bean
+    public AntPathMatcher antPathMatcher() {
+        return new AntPathMatcher();
+    }
+
+    @Bean
+    public Key key(JwtProperties jwtProperties) {
+        return Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes());
+    }
+}
