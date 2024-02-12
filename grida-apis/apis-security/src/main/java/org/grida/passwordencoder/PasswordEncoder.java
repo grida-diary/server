0b5338b4766fc69fd38949fb92f11b137d72693d
wwan13 <wwan13@naver.com>
2024-02-12 23:31:14 +0900

    refactor : PasswordEncoder 패키지 분리

---
+++ grida-apis/apis-security/src/main/java/org/grida/passwordencoder/PasswordEncoder.java
@@ -0,0 +1,20 @@
+package org.grida.passwordencoder;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PasswordEncoder {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public String encode(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
