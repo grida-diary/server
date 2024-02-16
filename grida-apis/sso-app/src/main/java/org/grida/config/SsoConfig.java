342b14bbe8c974ffc4c3f249b710b0750abc6dfd
wwan13 <wwan13@naver.com>
2024-02-16 22:50:35 +0900

    feat : 로깅, 인증/인가 필터 등록

---
+++ grida-apis/sso-app/src/main/java/org/grida/config/SsoConfig.java
@@ -0,0 +1,61 @@
+package org.grida.config;
+
+import org.grida.authorizedrequest.AuthorizedRequest;
+import org.grida.authorizedrequest.AuthorizedRequestBuilder;
+import org.grida.filter.AuthFilter;
+import org.grida.filter.AuthFilterExceptionHandler;
+import org.grida.filter.LogFilter;
+import org.grida.jwt.TokenDecoder;
+import org.grida.jwt.TokenValidator;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import static org.grida.authorizedrequest.AuthorizedRequestBuilder.Support.*;
+
+@Configuration
+public class SsoConfig {
+
+    @Bean
+    public FilterRegistrationBean<LogFilter> logFilter() {
+        FilterRegistrationBean<LogFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LogFilter());
+        registrationBean.setOrder(1);
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthFilterExceptionHandler> authFilterExceptionHandler() {
+        FilterRegistrationBean<AuthFilterExceptionHandler> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new AuthFilterExceptionHandler());
+        registrationBean.setOrder(2);
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthFilter> authFilter(
+            TokenDecoder tokenDecoder,
+            TokenValidator tokenValidator
+    ) {
+        FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+        AuthorizedRequest authorizedRequest = AuthorizedRequestBuilder.withPatterns()
+                .antMatchers(
+                        httpMethods(HttpMethod.POST),
+                        uriPatterns("/api/login", "/api/signup", "/api/signup/email"),
+                        permitAll()
+                )
+                .antMatchers(
+                        httpMethods(HttpMethod.GET),
+                        uriPatterns("/api/auth/role"),
+                        authenticated()
+                )
+                .elseRequestAuthenticated();
+        registrationBean.setFilter(new AuthFilter(tokenDecoder, tokenValidator, authorizedRequest));
+        registrationBean.setOrder(3);
+
+        return registrationBean;
+    }
+}
