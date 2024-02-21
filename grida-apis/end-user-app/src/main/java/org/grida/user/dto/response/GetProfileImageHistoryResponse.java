07f47839f23d7e0e15a5162f78b8a336a5979e78
wwan13 <wwan13@naver.com>
2024-02-22 00:50:15 +0900

    feat : 프로필 이미지 불러오 히스토리를 불러오는 API

---
+++ grida-apis/end-user-app/src/main/java/org/grida/user/dto/response/GetProfileImageHistoryResponse.java
@@ -0,0 +1,35 @@
+package org.grida.user.dto.response;
+
+import org.grida.CdnClient;
+import org.grida.domain.user.ProfileImage;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record GetProfileImageHistoryResponse(
+        List<HistoryElement> images
+) {
+
+    record HistoryElement(
+            long imageId,
+            String imageUrl,
+            LocalDateTime createdAt,
+            boolean isActivate
+    ) {
+    }
+
+    public static GetProfileImageHistoryResponse of(List<ProfileImage> images, CdnClient cdnClient) {
+        List<HistoryElement> elements = images.stream()
+                .map(image ->
+                        new HistoryElement(
+                                image.id(),
+                                cdnClient.getUrl(image.imageDetail().imagePath()),
+                                image.dateTime().createdAt(),
+                                image.imageDetail().isActivate()
+                        )
+                )
+                .toList();
+
+        return new GetProfileImageHistoryResponse(elements);
+    }
+}
