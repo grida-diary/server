f8f98460059763eac911940e7e8d23d91a11a7fd
wwan13 <wwan13@naver.com>
2024-02-24 01:38:42 +0900

    docs : User, ProfileImage API 문서 작성

--- grida-apis/end-user-app/src/test/java/org/grida/EndUserAppApplicationTests.java
+++ grida-apis/end-user-app/src/test/java/org/grida/EndUserAppApplicationTests.java
@@ -1,13 +1,7 @@
 package org.grida;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EndUserAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
