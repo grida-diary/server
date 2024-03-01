836aff06e6f9fc57d51c1419a61eb24f4e37f515
wwan13 <wwan13@naver.com>
2024-03-01 19:35:39 +0900

    feat : FeignClient 설정 추가

--- grida-domains/domain-ai/src/main/java/org/grida/config/DomainAiConfig.java
+++ grida-domains/domain-ai/src/main/java/org/grida/config/DomainAiConfig.java
@@ -1,9 +1,11 @@
 package org.grida.config;
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationPropertiesScan(basePackages = "org.grida")
+@EnableFeignClients(basePackages = "org.grida")
 public class DomainAiConfig {
 }
