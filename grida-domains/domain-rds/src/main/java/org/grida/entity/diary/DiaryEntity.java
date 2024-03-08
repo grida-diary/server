d0dd61d66b9543fe94b4121c2c058170138a2464
wwan13 <wwan13@naver.com>
2024-03-08 22:13:11 +0900

    feat : DiaryEntity, Mapper 구현

---
+++ grida-domains/domain-rds/src/main/java/org/grida/entity/diary/DiaryEntity.java
@@ -0,0 +1,46 @@
+package org.grida.entity.diary;
+
+import lombok.*;
+import org.grida.entity.base.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "diary")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class DiaryEntity extends BaseEntity {
+
+    @Column
+    private String userEmail;
+
+    @Column
+    private LocalDate targetDate;
+
+    @Column
+    private String content;
+
+    @Column
+    private Integer imageRefreshChance;
+
+    @Builder
+    public DiaryEntity(
+            LocalDateTime createdAt,
+            LocalDateTime lastModifiedAt,
+            String userEmail,
+            LocalDate targetDate,
+            String content,
+            Integer imageRefreshChance
+    ) {
+        super(createdAt, lastModifiedAt);
+        this.userEmail = userEmail;
+        this.targetDate = targetDate;
+        this.content = content;
+        this.imageRefreshChance = imageRefreshChance;
+    }
+}
