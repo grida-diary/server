8605c979e94c92f26f78f1c18a19ab686125669f
wwan13 <wwan13@naver.com>
2024-04-29 14:50:17 +0900

    refactor : 클래스 디렉토리 수정

---
+++ grida-domains/domain-image/src/main/java/org/grida/image/ImageMetaData.java
@@ -0,0 +1,26 @@
+package org.grida.image;
+
+import java.util.UUID;
+
+public record ImageMetaData(
+        String url,
+        String directory,
+        String name,
+        String extension
+) {
+
+    private static final String IMAGE_PATH_FORMAT = "%s/%s.%s";
+
+    public static ImageMetaData withRandomUuidFilename(String url, ImageType imageType) {
+        return new ImageMetaData(
+                url,
+                imageType.getDirectory(),
+                UUID.randomUUID().toString(),
+                imageType.getExtension()
+        );
+    }
+
+    public String toImagePath() {
+        return String.format(IMAGE_PATH_FORMAT, directory, name, extension);
+    }
+}
