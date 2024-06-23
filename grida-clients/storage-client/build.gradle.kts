018313fe0a99ef7ad8ef378f8d3cbcc0fe9e8885
wwan13 <wwan13@naver.com>
2024-06-24 00:43:15 +0900

    chore : kotest, mockk 의존성 추가

--- grida-clients/storage-client/build.gradle.kts
+++ grida-clients/storage-client/build.gradle.kts
@@ -2,12 +2,15 @@ dependencies {
     // spring boot
     implementation("org.springframework.boot:spring-boot-starter")
 
-    // lombok
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-
     // aws s3
     implementation("com.amazonaws:aws-java-sdk-s3:1.12.624")
+
+    // test
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.mockk:mockk:1.12.4")
 
     // core module
     implementation(project(":grida-core"))
--- grida-core/build.gradle.kts
+++ grida-core/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
-    // lombok
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+    // test
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
 }
