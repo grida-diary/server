c59fef8310db3bcb241d9daec2878fcc4bf50914
wwan13 <wwan13@naver.com>
2024-06-24 00:44:43 +0900

    chore : kts 문법 오류 수정

--- grida-domains/domain-rds/build.gradle.kts
+++ grida-domains/domain-rds/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("com.mysql:mysql-connector-j")
 
     // H2 for test
-    testImplementation 'com.h2database:h2'
+    testImplementation("com.h2database:h2")
 
     // module dependencies
     implementation(project(":grida-core"))
