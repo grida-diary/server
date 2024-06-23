545ace152b463c5a435032ee9aa86ba90f31a3b0
wwan13 <wwan13@naver.com>
2024-06-24 00:44:18 +0900

    chore : 마이그레이션 이전 java 파일 삭제

--- grida-clients/storage-client/src/main/java/org/grida/exception/StorageClientException.java
+++
@@ -1,8 +0,0 @@
-package org.grida.exception;
-
-public class StorageClientException extends BaseException {
-
-    public StorageClientException(ErrorCode errorCode, Object... args) {
-        super(errorCode, args);
-    }
-}
