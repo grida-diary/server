f55b0d9dbe762b87ae8a3d195cb0fd2d3bc8f52a
wwan13 <wwan13@naver.com>
2024-06-28 21:56:53 +0900

    feat : database-rds 모듈 설정

---
+++ grida-database/database-rds/src/main/kotlin/org/grida/databaserds/config/DatabaseRdsConfig.kt
@@ -0,0 +1,10 @@
+package org.grida.databaserds.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories
+class DatabaseRdsConfig
