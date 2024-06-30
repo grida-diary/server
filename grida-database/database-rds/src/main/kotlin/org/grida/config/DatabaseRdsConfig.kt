54021c2dca5c47c00998d4ce03cad60e62e58d17
wwan13 <wwan13@naver.com>
2024-07-01 01:21:25 +0900

    feat : jpa entity, transaction 설정

--- grida-database/database-rds/src/main/kotlin/org/grida/config/DatabaseRdsConfig.kt
+++ grida-database/database-rds/src/main/kotlin/org/grida/config/DatabaseRdsConfig.kt
@@ -1,10 +1,14 @@
-package org.grida.databaserds.config
+package org.grida.config
 
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories
+@EnableJpaAuditing
+@EntityScan(basePackages = ["org.grida"])
+@EnableJpaRepositories(basePackages = ["org.grida"])
 class DatabaseRdsConfig
