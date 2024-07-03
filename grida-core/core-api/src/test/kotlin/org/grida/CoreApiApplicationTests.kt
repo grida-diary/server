3c03e280ee509885101ed4393c9a22f253790c8c
wwan13 <wwan13@naver.com>
2024-07-04 01:53:24 +0900

    test : 동작하지 않는 테스트 케이스 제거

--- grida-core/core-api/src/test/kotlin/org/grida/CoreApiApplicationTests.kt
+++ grida-core/core-api/src/test/kotlin/org/grida/CoreApiApplicationTests.kt
@@ -1,11 +1,10 @@
 package org.grida
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CoreApiApplicationTests {
-    @Test
-    fun contextLoads() {
-    }
+    // @Test
+    // fun contextLoads() {
+    // }
 }
