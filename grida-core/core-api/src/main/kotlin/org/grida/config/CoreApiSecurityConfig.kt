80c9d1f0b1d4fd7b01552736fb5f94f3f1c7c6fa
wwan13 <wwan13@naver.com>
2024-07-25 14:34:04 +0900

    feat : security pattern 추가

--- grida-core/core-api/src/main/kotlin/org/grida/config/CoreApiSecurityConfig.kt
+++ grida-core/core-api/src/main/kotlin/org/grida/config/CoreApiSecurityConfig.kt
@@ -7,6 +7,7 @@ import io.wwan13.wintersecurity.config.JwtProviderConfigurer
 import io.wwan13.wintersecurity.config.SecureRequestConfigurer
 import io.wwan13.wintersecurity.jwt.support.JwtPropertiesRegistry
 import io.wwan13.wintersecurity.secretkey.support.SecretKeyRegistry
+import org.grida.domain.user.Role
 import org.springframework.context.annotation.Configuration
 
 @Configuration
@@ -30,9 +31,13 @@ class CoreApiSecurityConfig(
 
     override fun registerAuthPatterns(registry: AuthPatternsRegistry) {
         registry.apply {
-            uriPatterns("/api/v1/auth/user", "/api/v1/user")
+            uriPatterns("/api/v1/auth/login", "/api/v1/user")
                 .httpMethodPost()
                 .permitAll()
+
+            uriPatterns("/api/v1/user/image/**")
+                .allHttpMethods()
+                .hasRoles(Role.ROLE_USER)
 
             elseRequestAuthenticated()
         }
