ed92a8ca6bb06bfd9318e1bbc02060734300522d
wwan13 <wwan13@naver.com>
2024-07-25 22:44:01 +0900

    docs : 활성화된 프로필 이미지를 불러오는 API

--- grida-core/core-api/src/test/kotlin/org/grida/docs/profileimage/ProfileImageApiDocsTest.kt
+++ grida-core/core-api/src/test/kotlin/org/grida/docs/profileimage/ProfileImageApiDocsTest.kt
@@ -5,13 +5,18 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.runs
 import io.wwan13.api.document.snippets.BOOLEAN
+import io.wwan13.api.document.snippets.DATETIME
 import io.wwan13.api.document.snippets.ENUM
 import io.wwan13.api.document.snippets.NUMBER
 import io.wwan13.api.document.snippets.STRING
 import io.wwan13.api.document.snippets.isTypeOf
 import io.wwan13.api.document.snippets.whichMeans
 import org.grida.docs.ApiDocsTest
+import org.grida.domain.image.Image
+import org.grida.domain.image.ImageStatus
+import org.grida.domain.profileimage.Appearance
 import org.grida.domain.profileimage.Gender
+import org.grida.domain.profileimage.ProfileImage
 import org.grida.domain.profileimage.ProfileImageService
 import org.grida.presentation.v1.profileimage.ProfileImageController
 import org.grida.presentation.v1.profileimage.dto.GenerateSampleProfileImageRequest
@@ -123,6 +128,39 @@ class ProfileImageApiDocsTest(
             )
             responseFields(
                 "data.result" isTypeOf BOOLEAN whichMeans "확인 결과"
+            )
+        }
+    }
+
+    @Test
+    fun `활성화된 프로필 이미지를 불러오는 API`() {
+        val profileImage = ProfileImage(
+            id = 1L,
+            userId = 1L,
+            image = Image("https://imageUrl.com"),
+            appearance = Appearance(Gender.MAN, 21, "hair style", "glasses", "body shape")
+        )
+        every { profileImageService.readActivateProfileImage(any()) } returns profileImage
+
+        val api = api.get("/api/v1/user/image") {
+            withBearerToken()
+        }
+
+        documentFor(api, "profile-image-exists") {
+            summary("활성화된 프로필 이미지를 불러오는 API")
+            requestHeaders(
+                "Authorization" whichMeans "인증 토큰"
+            )
+            responseFields(
+                "data.imageId" isTypeOf NUMBER whichMeans "이미지 ID",
+                "data.imageUrl" isTypeOf STRING whichMeans "이미지 URL",
+                "data.status" isTypeOf ENUM(ImageStatus::class) whichMeans "이미지 활성화 상태",
+                "data.createdAt" isTypeOf DATETIME whichMeans "이미지 생성 시간",
+                "data.gender" isTypeOf ENUM(Gender::class) whichMeans "성별",
+                "data.age" isTypeOf NUMBER whichMeans "나이",
+                "data.hairStyle" isTypeOf STRING whichMeans "머리 스타일",
+                "data.glasses" isTypeOf STRING whichMeans "안경 유무 / 모양",
+                "data.bodyShape" isTypeOf STRING whichMeans "체형",
             )
         }
     }
