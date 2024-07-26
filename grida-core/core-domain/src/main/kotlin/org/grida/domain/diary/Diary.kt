4b0307ea7524dacfc6b271f40a3b0fec8dfd6ca9
wwan13 <wwan13@naver.com>
2024-07-26 22:53:02 +0900

    feat : diary 기본값 설정

--- grida-core/core-domain/src/main/kotlin/org/grida/domain/diary/Diary.kt
+++ grida-core/core-domain/src/main/kotlin/org/grida/domain/diary/Diary.kt
@@ -4,9 +4,9 @@ import org.grida.domain.base.Timestamp
 import java.time.LocalDate
 
 data class Diary(
-    val id: Long,
+    val id: Long = 0,
     val userId: Long,
-    val timestamp: Timestamp,
+    val timestamp: Timestamp = Timestamp(),
     val targetDate: LocalDate,
     val content: String,
     val scope: DiaryScope
