a3293ea632a87a7bd48fe0fa78353c4f949f4399
wwan13 <wwan13@naver.com>
2024-07-30 15:28:05 +0900

    refactor : 클래스 이름 중복 수정

--- grida-database/database-rds/src/main/kotlin/org/grida/persistence/diaryimage/DiaryImageJpaEntityRepository.kt
+++ grida-database/database-rds/src/main/kotlin/org/grida/persistence/diaryimage/DiaryImageJpaEntityRepository.kt
@@ -2,5 +2,5 @@ package org.grida.persistence.diaryimage
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface DiaryJpaEntityRepository : JpaRepository<DiaryImageEntity, Long> {
+interface DiaryImageJpaEntityRepository : JpaRepository<DiaryImageEntity, Long> {
 }
