d0ac832d40fb88e2d9241b945b9634fc5a23ce3b
wwan13 <wwan13@naver.com>
2024-07-30 15:26:35 +0900

    feat : (임시) diaryImageGenerator 임시 구현

---
+++ grida-core/core-domain/src/main/kotlin/org/grida/domain/diaryimage/DiaryImageGenerator.kt
@@ -0,0 +1,12 @@
+package org.grida.domain.diaryimage
+
+import org.springframework.stereotype.Component
+
+@Component
+class DiaryImageGenerator {
+
+    fun generate(diaryContent: String): String {
+        // TODO
+        return "https://tmpImageUrl.com"
+    }
+}
