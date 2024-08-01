756563195e921e3e4510bbfe8d2b4cc67a862f1d
wwan13 <wwan13@naver.com>
2024-08-01 19:08:57 +0900

    refactor : 에러코드 재사용 가능하게 수정

--- grida-core/core-domain/src/main/kotlin/org/grida/domain/profileimage/ProfileImageValidator.kt
+++ grida-core/core-domain/src/main/kotlin/org/grida/domain/profileimage/ProfileImageValidator.kt
@@ -1,7 +1,7 @@
 package org.grida.domain.profileimage
 
 import org.grida.domain.image.ImageStatus
-import org.grida.error.ActivateProfileImageAlreadyExists
+import org.grida.error.ActivateImageAlreadyExists
 import org.grida.error.GridaException
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -14,7 +14,7 @@ class ProfileImageValidator(
     @Transactional(readOnly = true)
     fun validateAlreadyHasActivateProfileImage(userId: Long) {
         if (profileImageRepository.existsByUserIdAndStatus(userId, ImageStatus.ACTIVATE)) {
-            throw GridaException(ActivateProfileImageAlreadyExists)
+            throw GridaException(ActivateImageAlreadyExists)
         }
     }
 }
