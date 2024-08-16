31bd4e1ca6587876e565658df4ab8721262b1314
wwan13 <wwan13@naver.com>
2024-08-17 01:04:08 +0900

    refactor : request logger 적용

--- grida-core/core-api/src/main/kotlin/org/grida/config/CoreApiConfig.kt
+++ grida-core/core-api/src/main/kotlin/org/grida/config/CoreApiConfig.kt
@@ -1,10 +1,27 @@
 package org.grida.config
 
-import org.grida.support.requestlogger.EnableLogRequest
+import io.wwan13.springrequestlogger.configure.EnableLoggingRequest
+import io.wwan13.springrequestlogger.configure.LogMessageConfigurer
+import io.wwan13.springrequestlogger.context.RequestContext
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
+import java.util.function.Function
 
 @Configuration
 @ConfigurationPropertiesScan
-@EnableLogRequest
-class CoreApiConfig
+@EnableLoggingRequest
+class CoreApiConfig : LogMessageConfigurer {
+
+    override fun format(): Function<RequestContext, String> {
+        return Function { context ->
+            """
+                |
+                | ${context.method} '${context.uri}' - ${context.status} (${context.elapsed} s)
+                | >> Request Headers : ${context.requestHeaders}
+                | >> Request Params  : ${context.requestParams}
+                | >> Request Body    : ${context.requestBody}
+                | >> Response Body   : ${context.responseBody}
+            """.trimMargin()
+        }
+    }
+}
