5102897d816c0f023a1e639ced48038fe81c9fdf
wwan13 <wwan13@naver.com>
2024-08-18 01:39:38 +0900

    feat : kakao client 설정

---
+++ grida-clients/kakao-client/src/main/kotlin/org/grida/config/KakaoClientConfig.kt
@@ -0,0 +1,10 @@
+package org.grida.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationPropertiesScan
+@EnableFeignClients(basePackages = ["org.grida"])
+class KakaoClientConfig
