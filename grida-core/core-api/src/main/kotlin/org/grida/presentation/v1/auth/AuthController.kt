3f4db611d711509ade08f5704dbd160f28a62ea2
wwan13 <wwan13@naver.com>
2024-08-18 00:58:10 +0900

    refactor : 인증 토큰 발급 로직 위치 변경

--- grida-core/core-api/src/main/kotlin/org/grida/presentation/v1/auth/AuthController.kt
+++ grida-core/core-api/src/main/kotlin/org/grida/presentation/v1/auth/AuthController.kt
@@ -1,9 +1,14 @@
 package org.grida.presentation.v1.auth
 
+import io.wwan13.wintersecurity.jwt.TokenGenerator
+import io.wwan13.wintersecurity.passwordencoder.PasswordEncoder
 import org.grida.api.ApiResponse
-import org.grida.auth.AuthTokenProvider
-import org.grida.auth.AuthTokens
+import org.grida.config.TokenPayload
+import org.grida.domain.user.UserService
+import org.grida.error.LoginFailed
 import org.grida.presentation.v1.auth.dto.LoginRequest
+import org.grida.presentation.v1.auth.dto.LoginResponse
+import org.grida.validator.requestvalidator.RequestValidator
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,14 +17,25 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/auth")
 class AuthController(
-    private val authTokenProvider: AuthTokenProvider
+    private val tokenGenerator: TokenGenerator,
+    private val userService: UserService,
+    private val passwordEncoder: PasswordEncoder
 ) {
 
     @PostMapping("/login")
     fun login(
         @RequestBody request: LoginRequest
-    ): ApiResponse<AuthTokens> {
-        val authTokens = authTokenProvider.provide(request.username, request.password)
-        return ApiResponse.success(authTokens)
+    ): ApiResponse<LoginResponse> {
+        val user = userService.findUser(request.username)
+        RequestValidator.validate(LoginFailed) {
+            passwordEncoder.matches(request.password, user.password)
+        }
+
+        val tokenPayload = TokenPayload(user.id, user.role)
+        val response = LoginResponse(
+            accessToken = tokenGenerator.accessToken(tokenPayload),
+            refreshToken = tokenGenerator.refreshToken(tokenPayload)
+        )
+        return ApiResponse.success(response)
     }
 }
