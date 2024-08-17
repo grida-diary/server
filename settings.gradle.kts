09869718b4afe1aa4716a55298ed9634e473b660
wwan13 <wwan13@naver.com>
2024-08-18 01:03:46 +0900

    chore : kakao-client 모듈 생셩

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "grida"
 include("grida-clients")
 include("grida-clients:storage-client")
 include("grida-clients:ai-client")
+include("grida-clients:kakao-client")
 
 include("grida-core")
 include("grida-core:core-api")
